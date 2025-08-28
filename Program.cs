6c7df1af8397b32ec7b671f979e33fa860e0a8b1
Ahmed Rashad <ahm.rashad.202@gmail.com>
2025-08-28 08:39:19 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Hello!");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+string userChoice = Console.ReadLine();
+Console.WriteLine(userChoice);
+
+Console.ReadKey();
